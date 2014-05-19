dc014430e6bb00a6a85ce1dba1ec3b2c71d6da81
Alex Moon <alex.moon@tangentlabs.co.uk>
2014-05-20 00:31:38 +0100

    Wanna consume JSON? Easy as pie

--- src/main/java/com/github/alex_moon/wick/Wick.java
+++ src/main/java/com/github/alex_moon/wick/Wick.java
@@ -1,5 +1,8 @@
 package com.github.alex_moon.wick;
 
+import java.util.Map;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -29,6 +32,16 @@ public class Wick {
 		// we want to create a new term if it doesn't exist, then upgrade it with
 		// the given score - this should all be doable with existing Controller methods
 		// @todo use POST body data for something
+		return controller.putTerm(term, score);
+	}
+	
+	@POST
+	@Path("/json")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Term putTermAsJson(Map<String, Object> termData) {
+		String term = (String) termData.get("term");
+		Double score = (Double) termData.get("score");
 		return controller.putTerm(term, score);
 	}
 
