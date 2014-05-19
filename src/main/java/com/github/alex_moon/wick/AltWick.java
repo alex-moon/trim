7b79b723f691032008e945178db5a758a590514d
Alex Moon <alex.moon@tangentlabs.co.uk>
2014-05-20 00:48:51 +0100

    Consume POST body on v1 too :D muhahahaha

--- src/main/java/com/github/alex_moon/wick/AltWick.java
+++ src/main/java/com/github/alex_moon/wick/AltWick.java
@@ -1,9 +1,11 @@
 package com.github.alex_moon.wick;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -29,27 +31,38 @@ public class AltWick extends HttpServlet {
             throws IOException, ServletException {
 		PrintWriter out = res.getWriter();
         String url = req.getRequestURL().toString();
-        if (url != null) {
-            res.setContentType("application/json");
-            List<String> url_path = Arrays.asList(url.split("/"));
-            String termString = url_path.get(url_path.size()-2);
-            Double score = Double.parseDouble(url_path.get(url_path.size()-1));
-            Term term = controller.putTerm(termString, score);
-            out.println(JSONValue.toJSONString(term.toJson()));
-        }
-        out.close();
+        res.setContentType("application/json");
+        List<String> url_path = Arrays.asList(url.split("/"));
+
+		String termString;
+		Double score;
+		String lastToken = url_path.get(url_path.size()-1);
+		if (lastToken.equals("json")) {
+			BufferedReader reader = req.getReader();
+			Map<String, Object> jsonObject = (Map<String, Object>) JSONValue.parse(reader);
+			termString = (String) jsonObject.get("term");
+			score = (Double) jsonObject.get("score");
+		} else {
+			termString = url_path.get(url_path.size()-1);
+			score = Double.parseDouble(lastToken);
+		}
+
+		Term term = controller.putTerm(termString, score);
+		out.println(JSONValue.toJSONString(term.toJson()));
+		out.close();
 	}
 
     public void doGet(HttpServletRequest req, HttpServletResponse res)
                     throws IOException, ServletException {
         PrintWriter out = res.getWriter();
         String url = req.getRequestURL().toString();
-        if (url != null) {
-            res.setContentType("application/json");
-            List<String> url_path = Arrays.asList(url.split("/"));
-            Term term = controller.getTerm(url_path.get(url_path.size()-1));
-            out.println(JSONValue.toJSONString(term.toJson()));
-        }
+        res.setContentType("application/json");
+        List<String> url_path = Arrays.asList(url.split("/"));
+
+        String termString = url_path.get(url_path.size()-1);
+
+        Term term = controller.getTerm(termString);
+        out.println(JSONValue.toJSONString(term.toJson()));
         out.close();
     }
 }
