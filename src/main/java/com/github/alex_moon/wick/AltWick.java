16d63b9c47e10e50b1ba0089f0e61da36093790a
Alex Moon <alex.moon@tangentlabs.co.uk>
2014-05-20 00:09:58 +0100

    And here's POST - easy peasy

--- src/main/java/com/github/alex_moon/wick/AltWick.java
+++ src/main/java/com/github/alex_moon/wick/AltWick.java
@@ -17,16 +17,27 @@ import com.github.alex_moon.wick.term.Term;
 
 public class AltWick extends HttpServlet {
 	private Controller controller;
-	
+
 	public void init() {
 		if (controller == null) {
 			controller = new Controller();
 			controller.start();
 		}
 	}
-	
-	public void doPost(HttpServletRequest req, HttpServletResponse res) {
-		
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws IOException, ServletException {
+		PrintWriter out = res.getWriter();
+        String url = req.getRequestURL().toString();
+        if (url != null) {
+            res.setContentType("application/json");
+            List<String> url_path = Arrays.asList(url.split("/"));
+            String termString = url_path.get(url_path.size()-2);
+            Double score = Double.parseDouble(url_path.get(url_path.size()-1));
+            Term term = controller.putTerm(termString, score);
+            out.println(JSONValue.toJSONString(term.toJson()));
+        }
+        out.close();
 	}
 
     public void doGet(HttpServletRequest req, HttpServletResponse res)
